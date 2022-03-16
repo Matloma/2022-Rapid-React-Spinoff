5738a8dd3fbfa51b657690ea237a9125ddde2a68
Matloma <leodan.villegas-avina@tagmagnet.org>
2022-03-16 17:05:41 -0500

    Started Climber code

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,12 +4,30 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
+
+  TalonFX telescopingMotor;
+  Solenoid modeSelector;
+  boolean FAST;
+
+
   /** Creates a new Climber. */
   public Climber() {
-    
+    telescopingMotor = new TalonFX(Constants.telescoping_motor_port);
+    modeSelector = new Solenoid(PneumaticsModuleType.REVPH, Constants.telescoping_power_mode_solenoid_port);
+    FAST = true;
+  }
+
+  public void moveArms(double speed){
+    telescopingMotor.set(TalonFXControlMode.Position, telescopingMotor.getSelectedSensorPosition()+speed);
   }
 
   @Override
